72eece5e4e480cb7fae2a1ae15333bf0e27bce0b
anandprabhu530 <anandprabhu530@gmail.com>
2024-03-26 22:20:24 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import { SetStateAction, useState } from "react";
 
 export default function Home() {
   const [selectedValue, setSelctedValue] = useState("");
-  const [data,setData] = useState({
-    recommendations: [],
-    specific: "",
-    preffered:""
-  })
+  const [data,setData] = useState(null)
 
   const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
     setSelctedValue(event.target.value)
@@ -19,7 +15,7 @@ export default function Home() {
   const handleSubmit = async(event: { preventDefault: () => void; }) => {
     event.preventDefault();
     const res = await analyze_data(selectedValue);
-    console.log(res)
+    setData(res);
   }
   
   return (
@@ -32,6 +28,10 @@ export default function Home() {
         <form className="flex flex-col gap-4">
           <textarea className="w-[500px] h-[300px] bg-transparent outline-none resize-none border-2 border-white rounded-lg p-4 text-xl" onChange={handleChange}/>
           <button className="px-4 py-2 bg-blue-400" onClick={handleSubmit}>Submit</button>
+          <button className="px-4 py-2 bg-blue-400 ml-6" onClick={() => {
+            setData(null)
+          }}>Clear</button>
+
         </form>
       </div>
         {data.recommendations.length != 0 && <div>
---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+const Resume = () => {
+  return (
+    <div>Generate resume. </div>
+  )
+}
+
+export default Resume
