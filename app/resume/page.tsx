ddbe96dd84181dc994dee10a46efeb5100410aff
anandprabhu530 <anandprabhu530@gmail.com>
2024-03-30 22:59:25 +0530

    Resume review boilerplate

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,32 @@
-const Resume = () => {
-  return (
-    <div>Generate resume. </div>
-  )
-}
+"use client";
 
-export default Resume
+import { analyze_data } from "@/utils/ai";
+import { useState } from "react";
+
+const Resume = () => {
+  const [data, setData] = useState("");
+
+  const handlesubmit = async (event) => {
+    event.preventDefault();
+    const res = await analyze_resume();
+    setData(res);
+  };
+  return (
+    <main>
+      <div className="border-2 border-black p-8  ">
+        <div className="text-4xl font-semibold">Get your Resume Reviewed</div>
+        <div className="pt-8 flex flex-col gap-2">
+          Choose your Resume:
+          <input type="file" />
+        </div>
+        {data.length != 0 && (
+          <div>
+            <div>You Resume needs to be developed</div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Resume;
--- utils/ai.ts
+++ utils/ai.ts
@@ -1,28 +1,44 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { StructuredOutputParser } from "langchain/output_parsers";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
-import { z } from "zod"
+import { z } from "zod";
 
 const parser = StructuredOutputParser.fromZodSchema(
-    z.object({
-        recommendations: z.array(z.string()).describe("Give the career that can be suitable for the given answers only"),
-        specific: z.string().describe("Give the one carrer path that is suitable for the person"),
-        preffered:z.string().describe("Give the top trending jo in his requirement"),
-    })
-)
+  z.object({
+    recommendations: z
+      .array(z.string())
+      .describe(
+        "Give the career that can be suitable for the given answers only"
+      ),
+    specific: z
+      .string()
+      .describe("Give the one carrer path that is suitable for the person"),
+    preffered: z
+      .string()
+      .describe("Give the top trending jo in his requirement"),
+  })
+);
 
-export const analyze_data = async (input:string) => {
-    const chain = RunnableSequence.from([
-        PromptTemplate.fromTemplate(
-            "Answer the users question as best as possible.\n{format_instructions}\n{question}"
-        ),
-        new ChatGoogleGenerativeAI({ apiKey:process.env.NEXT_PUBLIC_GOOGLE_API_KEY,modelName: "gemini-pro", maxOutputTokens: 2048 }),
-        parser,
-    ]);
-    const response = await chain.invoke({
-        question: input,
-        format_instructions: parser.getFormatInstructions(),
-    });
-    return response
-} 
+export const analyze_data = async (input: string) => {
+  const chain = RunnableSequence.from([
+    PromptTemplate.fromTemplate(
+      "Answer the users question as best as possible.\n{format_instructions}\n{question}"
+    ),
+    new ChatGoogleGenerativeAI({
+      apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+      modelName: "gemini-pro",
+      maxOutputTokens: 2048,
+    }),
+    parser,
+  ]);
+  const response = await chain.invoke({
+    question: input,
+    format_instructions: parser.getFormatInstructions(),
+  });
+  return response;
+};
+
+export const analyze_resume = async () => {
+  return "Successful";
+};
